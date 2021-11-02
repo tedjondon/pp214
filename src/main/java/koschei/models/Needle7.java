e3bcabac4b0047e5e26bdc8ad01550230163c464
Ted Jondon <ted.jondon@yandex.ru>
2021-11-02 20:10:22 +0300

    ready

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 deth;
 
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
+    }
+
+    @Autowired
+    public void setDeath(Deth8 death) {
+        this.deth = death;
     }
 }
